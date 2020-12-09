7bee1de4d9e3c8cdcc68ccd5b9cefbf2510feb54
Adam Hartley <git@ahartley.com>
2020-12-09 12:51:10 +0000

    Parallel jobs

--- browser_config.js
+++ browser_config.js
@@ -1,3 +1,7 @@
-let browsers = ['chrome', 'firefox'];
+if(process.env.CIRCLECI) {
+  let browsers = [process.argv[10]];
+} else {
+  let browsers = ['chrome', 'firefox'];
+}
 
 module.exports.browsers = browsers;
